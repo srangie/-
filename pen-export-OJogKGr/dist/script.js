95a3ac43b09975b4457f4334fa5f271082808027
srangie <130960491+srangie@users.noreply.github.com>
2023-05-29 13:53:09 +0800

    Add files via upload

--- pen-export-OJogKGr/dist/script.js
+++ pen-export-OJogKGr/dist/script.js
@@ -1,1 +1,8 @@
 document.write("HELLO未滿18禁止進入喔 ")
+var count = 0;
+    var counterElement =  document.getElementById("counter");
+
+    function increment() {
+      count+=2;
+      counterElement.textContent = count;
+    }
